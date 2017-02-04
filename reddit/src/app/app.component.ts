8260430d232759c269df5e73495ba9dd5bb917bf
jesse <jsoldat@hotmail.com>
2017-02-04 13:44:07 +0700

    can add articles with the form

--- reddit/src/app/app.component.ts
+++ reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
 
-  addArticle(title: HTMLInputElement, link: HTMLInputElement){
-  	console.log(`${title.value} / ${link.value}`);
+	articles: Article[];
+ 	
+ 	constructor(){
+ 		this.articles = [
+ 			new Article('Angular CLI', 'https://cli.angular.io/', 8),
+ 			new Article('Fullstack', 'http://fullstack.io', 5),
+ 			new Article('React Native', 'https://facebook.github.io/react-native/', 16),
+ 			new Article('CSS Tricks', 'https://css-tricks.com/snippets/javascript/get-url-and-url-parts-in-javascript/', 9)
+
+ 		];
+ 	}
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+  	// console.log(`${title.value} / ${link.value}`);
+  	this.articles.push(new Article(title.value, link.value, 0));
+  	title.value = '';
+  	link.value = '';
   	return false;
+  }
+
+  sortedArticles(): Article[] {
+  	return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
  
